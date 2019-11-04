29ed2895ca6fb50d4df3a9506ff2ad0757414044
AlaguSelvan <alaguselvan97@gmail.com>
2019-11-04 19:14:57 +0530

    sorter with string

--- src/index.ts
+++ src/index.ts
@@ -1,30 +1,32 @@
-interface swap {
-  arr: number[];
-  i: number;
-  j: number;
-}
-
 class Sorter {
 
-  constructor(public collection: number[]) {
+  constructor(public collection: number[] | string) {
     this.collection = collection;
   }
 
   private swap(arr:number[], i: number, j: number): void {
-    [arr[i], arr[j]] = [arr[j], arr[i]]
+    [arr[i], arr[j]] = [arr[j], arr[i]];
   }
 
   public bubbleSort(): void {
     const { length } = this.collection;
+    
     let noSwaps: Boolean
     for(let i = 0; i < length; i++) {
       noSwaps = false
       if(!noSwaps) {
       for( let j = 0; j < length - 1; j++) {
-        if(this.collection[j] > this.collection[j + 1]) {
-            noSwaps = true
-            this.swap(this.collection, j, j + 1);
-          }
+        // Bad Code
+        // Type Guards
+        if(this.collection instanceof Array) {
+          if(this.collection[j] > this.collection[j + 1]) {
+              noSwaps = true
+              this.swap(this.collection, j, j + 1);
+            }
+        }
+        if(typeof this.collection === 'string') {
+          // this.collection
+        }
         }
       }
     }
